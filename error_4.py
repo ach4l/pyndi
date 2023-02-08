eb8122ce919512bd92b5799df48e7149e504ed32
Achal <achal.agrawal.1987@gmail.com>
2023-02-08 18:30:31 +0530

    Moved output window to second place

---
+++ error_4.py
@@ -0,0 +1,8 @@
+jaan = 100      # tum mein kitni jaan hai?
+while jaan > 0:
+    print "str_0"
+    if jaan > 50:
+        jaan ko 2 se bhaag karo
+    else:
+        jaan ko 10 se ghatao
+    print jaan
